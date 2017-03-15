6ddf0ba314444d8313a4662f5d9f41a357ad50bc
Murilo Erhardt <murilo.erhardt@pucpr.edu.br>
2017-03-14 21:08:41 -0300

    init

---
+++ Files/LibraryServer/src/libraryserver/Livro.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package libraryserver;
+
+/**
+ *
+ * @author murilo.erhardt
+ */
+public class Livro {
+    private int code;
+    private String title;
+    private String year;
+    private String usrCode;
+
+    
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    public String getUsrCode() {
+        return usrCode;
+    }
+
+    public void setUsrCode(String usrCode) {
+        this.usrCode = usrCode;
+    }
+}
